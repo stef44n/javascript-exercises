3e8fe1dc7d2b8aa2a9f67b333b2a174e9c7ae6cd
Stefan <stefanseifeddine@gmail.com>
2022-06-14 21:08:50 +0100

    Add my solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let text = '';
+    let zero = 0;
+    function stringRepeat() {
+        if (num < 0) {
+            return 'ERROR'
+        }
+        while (zero < num) {
+            text += string;
+            zero++;
+        }
+        return text;
+    }
+        return stringRepeat();
 };
 
 // Do not edit below this line
