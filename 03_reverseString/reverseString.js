153b4c946985f3c0f348547bbfbed1a3ea4dbb22
Stefan <stefanseifeddine@gmail.com>
2022-06-16 17:12:22 +0100

    Add test solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let stringArray = string.split('');
+    let sLen = stringArray.length;
+    let newArr = [];
 
+    for (let i = sLen - 1; i >=0; i--) {
+        let popped = stringArray.pop()
+        newArr.push(popped)
+    }
+
+    let reversedStr = newArr.join('')
+    return reversedStr
 };
 
 // Do not edit below this line
