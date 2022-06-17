7d88532310ad2c0f872f7fc8801c53c4a099b5a7
Stefan <stefanseifeddine@gmail.com>
2022-06-17 23:19:31 +0100

    Add my full solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,6 +3,12 @@ const sumAll = function(num1, num2) {
     let integers = diff + 1;
     let answer = ((num1 + num2)*integers)/2;
 
+    if (num1 < 1 || num2 < 1 ||
+        Number.isInteger(num1) !== true || 
+        Number.isInteger(num2) !== true) {
+            return 'ERROR'
+    }
+
     return answer
 
 };
