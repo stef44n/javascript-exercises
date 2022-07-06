4a67b620a74bdbd222fec7227ec3aff70c3ff298
Stefan <stefanseifeddine@gmail.com>
2022-07-06 16:55:54 +0100

    Add test 1 solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function(string) {
+    let stringArray = string.split('');
+    let sLen = stringArray.length;
+    let newArr = [];
 
+    for (let i = sLen - 1; i >=0; i--) {
+        let popped = stringArray.pop()
+        newArr.push(popped)
+    }
+
+    let reversedStr = newArr.join('')
+
+    if (reversedStr === string) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
