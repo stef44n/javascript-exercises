924b14cdec23f5517f21b1b4ff2fa01854ed87cb
Stefan <stefanseifeddine@gmail.com>
2022-07-06 20:42:04 +0100

    Add solution to tests 1-5

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    const init = [0, 1]
 
+    function fibb(num) {
+        for (let i = 0; i < num - 1; i++) {
+            let newArr = Number(init[i] + init[i + 1]);
+            init.push(newArr);
+        }
+    }
+    fibb(num)
+    return init[num]
 };
 
 // Do not edit below this line
