6fa573ca6bdcbd9e9e50a0feba2e515faeefbd4d
Stefan <stefanseifeddine@gmail.com>
2022-07-06 20:48:18 +0100

    Add my final solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,10 @@
 const fibonacci = function(num) {
     const init = [0, 1]
 
+    if (num < 0) {
+        return 'OOPS'
+    }
+
     function fibb(num) {
         for (let i = 0; i < num - 1; i++) {
             let newArr = Number(init[i] + init[i + 1]);
