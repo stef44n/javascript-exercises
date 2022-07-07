1c96b7a519e5f577e8bd2af942faae17cdcf7f04
Stefan <stefanseifeddine@gmail.com>
2022-07-07 15:55:34 +0100

    Add test 1 solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+    let pLen = people.length
+
+    function age(people) {
+        for (let i = 0; i < pLen; i++) {
+            people[i]['age'] = people[i]['yearOfDeath'] - people[i]['yearOfBirth']
+        }
+    }
+    age(people)
+
+    const ordered = people.sort(function(a, b) {
+      if (a.age < b.age) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+
+    return people[0]
 
 };
 
